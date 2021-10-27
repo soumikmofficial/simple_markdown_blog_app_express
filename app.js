231c0b7e0ccc8260c091ad8803b380b4bcead9bf
soumikm.official@gmail.com <soumikmofficial@gmail.com>
2021-10-27 11:28:53 +0530

    routes initialized

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+const articleRouter = require("./routes/articles");
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -9,6 +10,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.render("home", { title: "Home Page" });
 });
+
+app.use("/articles", articleRouter);
 
 const port = process.env.PORT || 3000;
 
