ac06eb900ac618235f9f26e36fe87dd0311ddbef
soumikm.official@gmail.com <soumikmofficial@gmail.com>
2021-10-27 13:39:03 +0530

    basic styling added

--- app.js
+++ app.js
@@ -7,9 +7,18 @@ const articleRouter = require("./routes/articles");
 
 app.set("view engine", "ejs");
 app.use(express.json());
+app.use(express.static("public"));
+
+const data = [
+  {
+    title: "First Article",
+    date: new Date().toLocaleString(),
+    description: "This is the very first article written for trial mode",
+  },
+];
 
 app.get("/", (req, res) => {
-  res.render("home", { title: "Home Page" });
+  res.render("home", { title: "Home Page", articles: data });
 });
 
 app.use("/articles", articleRouter);
--- controllers/articles.js
+++ controllers/articles.js
@@ -1,5 +1,13 @@
+const data = [
+  {
+    title: "First Article",
+    date: new Date().toLocaleDateString(),
+    description: "This is the very first article written for trial mode",
+  },
+];
+
 const getAllArticles = async (req, res) => {
-  res.status(200).render("articles");
+  res.status(200).render("articles", { title: "articles", articles: data });
 };
 
 module.exports = {
