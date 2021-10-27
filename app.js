7995b9773f8509082e89b2db1a38b3137285a0e5
soumikm.official@gmail.com <soumikmofficial@gmail.com>
2021-10-27 10:52:36 +0530

    server running

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("workng");
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+// const ejs = require("ejs");
+
+// app.set("view engine", ejs);
+app.use(express.json());
+
+// app.get("/", (req, res) => {
+//   res.render("home");
+// });
+
+app.get("/", (req, res) => {
+  res.send("working");
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`server is running on port ${port}`));
