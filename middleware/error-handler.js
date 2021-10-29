929360d15b2451b049e6cf432910bd8c2a97d5c0
soumikm.official@gmail.com <soumikmofficial@gmail.com>
2021-10-30 02:45:18 +0530

    minor features styling and button

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,7 +1,5 @@
 const errorHandler = (err, res, req, next) => {
-  if (err.message === "not found") {
-    res.render(404);
-  }
-  res.status(err.status || 500).json({ status: "unsuccessful", message: err });
+  console.log(err);
+  res.status(500).json({ status: "unsuccessful", message: err });
 };
 module.exports = errorHandler;
